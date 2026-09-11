89be1f6ea4375557f8970e8bec78459b2769add6
pookiedookie69 <128asasina@frhsd.com>
2026-09-11 09:37:59 -0400

    second reply to broskis

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("H.S.");
         System.out.println("Hi, H.S., I am RP");
+        System.out.print("hi RP, HS. I'm AVS");
     }
 }
